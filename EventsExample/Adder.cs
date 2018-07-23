cbf56bb934139da83935260f6d39c64a49652cd5
Jenney <jenney.b28@yahoo.com>
2018-07-23 15:48:18 +0200

    commit 1

---
+++ EventsExample/Adder.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsExample
+{
+   public class Adder
+
+    {
+        public event EventHandler<MultipleOfFiveEventArgs> OnMultipleOfFiveReached;
+        public int Add(int x, int y)
+        {
+            int iSum = x + y;
+            if ((iSum % 5 == 0) && (OnMultipleOfFiveReached != null))
+            {
+                OnMultipleOfFiveReached
+                    (this, new MultipleOfFiveEventArgs(iSum));
+
+
+
+
+            }
+            return iSum;
+
+
+        }
+
+    }
+}
---
+++ EventsExample/MultipleOfFiveEventArgs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsExample
+{
+   public  class MultipleOfFiveEventArgs : EventArgs
+
+    {
+        public MultipleOfFiveEventArgs(int iTotal) {
+
+
+            Total = iTotal;
+
+        }
+         public int Total { get; set;  }
+    }
+}
